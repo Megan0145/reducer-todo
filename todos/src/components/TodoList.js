137db43e5dedb2d1cc37959a824253a8c499eb76
Megan <mennis0145@gmail.com>
2019-10-08 21:55:06 +0100

    Toggle complete case added to reducer, function created to toggle color of todo dependant on its completed state

--- todos/src/components/TodoList.js
+++ todos/src/components/TodoList.js
@@ -2,10 +2,18 @@ import React from "react";
 
 export default function TodoList(props) {
   const { state, dispatch } = props;
+
+  const toggleTodo = (id) => {
+      dispatch({
+          type: 'ON_TOGGLE_COMPLETE',
+          payload: {id}
+      });
+  }
+
   return (
     <div>
       {state.todos.map(todo => {
-        return <p>{todo.name}</p>;
+        return <p className={todo.completed ? 'complete' : 'incomplete'} onClick={() => toggleTodo(todo.id)}>{todo.name}</p>;
       })}
     </div>
   );
--- todos/src/reducers/reducer.js
+++ todos/src/reducers/reducer.js
@@ -1,5 +1,6 @@
 export const ON_INPUT_CHANGE = "ON_INPUT_CHANGE";
 export const ON_ADD_TODO = "ON_ADD_TODO";
+export const ON_TOGGLE_COMPLETE = "ON_TOGGLE_COMPLETE";
 
 export function reducer(state, action) {
   switch (action.type) {
@@ -12,6 +13,16 @@ export function reducer(state, action) {
       return {
         ...state,
         todos: [...state.todos, action.payload]
+      };
+    case ON_TOGGLE_COMPLETE:
+      return {
+        ...state,
+        todos: state.todos.map(todo => {
+          if (todo.id === action.payload.id) {
+            return { ...todo, completed: !todo.completed };
+          }
+          return todo;
+        })
       };
     default:
       return state;
